9e7ea72d9b996d7873c35531383714fff7547249
aphremthomas <aphremthomas@gmail.com>
2025-07-22 08:10:28 -0400

    update delete and add of jobs done

--- server.py
+++ server.py
@@ -24,9 +24,10 @@ load_dotenv()
 
 app = Flask(__name__)
 mail = Mail(app) # instantiate the mail class 
-app.config["JWT_COOKIE_SECURE"] = False
+app.config["JWT_COOKIE_SECURE"] = True
 app.config["JWT_TOKEN_LOCATION"] = ["cookies"] 
 app.config["JWT_SECRET_KEY"] = os.getenv('SIGN_KEY')
+app.config['JWT_ALGORITHM'] = 'HS256'
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
@@ -34,9 +35,18 @@ app.config['MAIL_USERNAME'] = 'jobs4ottawa@gmail.com'
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASS")
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
+app.config['JWT_COOKIE_SAMESITE'] = 'None'
 mail = Mail(app) 
 
 jwt = JWTManager(app)
+
+# Handle unauthorized access by unsetting JWT cookies
+@jwt.unauthorized_loader
+def unauthorized_callback(callback):
+    print("unauthorized callback called")
+    resp = make_response(jsonify({"msg": "Missing or invalid token"}), 401)
+    unset_jwt_cookies(resp)
+    return resp
 
 sign_key = os.getenv('SIGN_KEY')
 print("sign key", sign_key)
@@ -66,15 +76,22 @@ def allowed_file(filename):
 dbname = get_database()
 jobsCollection = dbname['jobs']
 
+
+@app.before_request
+def before_request():
+    print("Request Headers:", request.headers)
+    print("Request Cookies:", request.cookies)
+    print('Request formdata', request.form)
+
 @app.route("/login", methods=['POST'])
 def login():
     user_name = request.form.get('username')
     password = request.form.get('password')
     print("received user name:", user_name)
     print("received pass:", password)
-    print("is matching", user_name=='admin-sava' and password == 'abc')
+    print("is matching", user_name=='abc' and password == 'abc')
     resp = make_response(jsonify({"message":"howdi partner..."}))
-    if user_name=='admin-sava' and password == 'abc':
+    if user_name=='abc' and password == 'abc':
         access_token = create_access_token(identity=user_name)
         print("encoded jwt", access_token)
         set_access_cookies(resp, access_token)
@@ -94,12 +111,16 @@ def logout():
 @app.route("/jobs", methods=['POST'])
 @jwt_required()
 def add_job():
+    print("req-->",request.form)
+    print(request.cookies)
     location = request.form.get('location')
     role = request.form.get('role')
     description = request.form.get('description')
     date = request.form.get('date')
     status = request.form.get('status')
     title = request.form.get('title')
+    company = request.form.get('company')
+    requirements = request.form.get('requirements').split(';')
     try:
         if(jobsCollection is not None):
             jobsCollection.insert_one(
@@ -109,7 +130,9 @@ def add_job():
                     "description":description,
                     "date":date,
                     "status":status,
-                    "title":title
+                    "title":title,
+                    "company":company,
+                    "requirements":requirements,
                 }
             )
     except Exception as e:
@@ -147,8 +170,25 @@ def get_jobs():
     return  json.dumps(bson.json_util.dumps(outp)), '200'
 
 @app.route("/job/<id>", methods=['GET'])
-def get_job():
-    return {}, '200'
+def get_job(id):
+    jobDetail = jobsCollection.find_one({'_id': bson.ObjectId(id)})
+    return json.dumps(bson.json_util.dumps(jobDetail)), '200'
+
+@app.route("/job/<id>", methods=['DELETE'])
+@jwt_required()
+def delete_job_by_id(id):
+    print("deleting job with id", id)
+    try:
+        if(jobsCollection is not None):
+            jobsCollection.delete_one(
+               {
+                    "_id":bson.ObjectId(id),
+                }
+            )
+    except Exception as e:
+        print("error in deleting", e)
+        return '500'
+    return '200'
 
 #this route for applying for job and submitting resume
 @app.route("/apply", methods=['POST'])
@@ -156,16 +196,18 @@ def apply_job():
     fname = request.form.get('fname')
     lname = request.form.get('lname')
     email = request.form.get('email')
-    jobtitle = request.form.get('jobtitle')#current job title of the applicant.
+    jobtitle = request.form.get('jobtitle') #current job title of the applicant.
     status = request.form.get('status')
+    experience = request.form.get('experience')
     jobid = request.form.get('jobid') #job id to which he is applying
     job_name = getattr(jobsCollection.find_one({'_id':bson.ObjectId(jobid)},{"title":1}),'title', None) #name of job which he is applying
     print("jobname",job_name, jobid)
-    if (job_name is None):
+    print("fileee", request.files)
+    if (jobid is not None and job_name is None):
         return {"message":"invalid job id"}, '404'
-    if 'file' not in request.files:
+    if 'resume' not in request.files:
         return {"message":"no files attached"}, '404'
-    file = request.files['file']
+    file = request.files['resume']
     # If the user does not select a file, the browser submits an
     # empty file without a filename.
     if file.filename == '':
@@ -188,7 +230,7 @@ def apply_job():
                     recipients = ['jobs4ottawa@gmail.com'] 
                     
                 ) 
-        msg.html = f"<h3>Job application of {fname+ ' ' + lname} for {job_name}.</h3><br/> Contact email: {email}<br/> Current job title: {jobtitle} <br/> Status: {status}"
+        msg.html = f"<h3>Job application of {fname+ ' ' + lname} for {job_name}.</h3><br/> Contact email: {email}<br/> Current job title: {jobtitle} <br/> Status: {status} <br/> Experience: {experience}"
         msg.attach(file.filename,None,file.read())
         mail.send(msg) 
         return {}, '200'
